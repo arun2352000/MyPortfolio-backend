32bfd5d6f8b364accfe04308d9ff59164070fd9c
arun2352000 <poorvaja10@gmail.com>
2024-10-19 08:07:17 +0530

    update

--- Models/contact.Schema.js
+++ Models/contact.Schema.js
@@ -8,7 +8,13 @@ const contactSchema = new mongoose.Schema(
             },
             email: {
                 type: String,
-                required: true
+                required: true,
+                // unique: true,
+                // match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Please fill a valid email address'],
+                // index: true
+            
+            
+
                 },
                 phone: {
                     type: String
