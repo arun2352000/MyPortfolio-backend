af5a07fcab456f0b3af14bf1462462ecfd45a8eb
arun2352000 <poorvaja10@gmail.com>
2025-01-03 15:04:47 +0530

    schema update

--- Models/contact.Schema.js
+++ Models/contact.Schema.js
@@ -13,13 +13,25 @@ const contactSchema = new mongoose.Schema(
                 // match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Please fill a valid email address'],
                 // index: true
             
-            
-
+                },
+                isemailverified:{
+                    type:Boolean,
+                    default:false
+                },
+                isemailkey:{
+                    type:String
                 },
                 phone: {
                     type: String,
                     required: true
                 },
+                isphoneverified:{
+                    type:Boolean,
+                    default:false
+                    },
+                    isphonekey:{
+                        type:String
+                        },
                 message:{
                     type:String,
                     required:true,
